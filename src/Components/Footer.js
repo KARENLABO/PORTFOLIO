5a06d2c14575a732dcfc0d4052d2e393c9ccda01
Karen Ramirez <karen.bussines@hotmail.com>
2020-05-08 23:37:56 -0500

    se crean los componentes de Footer y Profile

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return(
+        <div>
+        <p> All rights reserved to Karen Ramirez ©℗®™</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Components/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Profile() {
+    return(
+        <div>
+            <img alt='ProfilePhoto' src='https://avatars1.githubusercontent.com/u/56927449?s=460&u=c5ed73b31461d885ea253348676b9c0433b92e4d&v=4'/>   
+        <h2>Ana Karen Ramirez</h2>
+        <p> Jr. front End Developer</p>
+        </div>
+    )
+}
+
+export default Profile
