341eff7a1977332839f07b9923aa2b73f5121474
Karen Ramirez <karen.bussines@hotmail.com>
2020-05-10 21:05:46 -0500

    se termina ddiseño de compponente MAINPRIMARY

--- src/Components/MainPrimary/MainPrimary.js
+++ src/Components/MainPrimary/MainPrimary.js
@@ -4,13 +4,13 @@ import './MainPrimary.css'
 function MainPrimary() {
     return (
         <div className='principalHome'>
-            <div >
+            <div className='flowersLeft'  >
                 <img className='principalHomeFlowersimg' alt='flowersLeft' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20LEFT.png?raw=true' />
             </div>
             <div className='logoprincipalHome'>
-                <img alt='ProfilePhoto' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/LOGO%20GRANDE%20.png?raw=true' />
+                <img alt='ProfilePhoto' className='ProfilePhoto' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/LOGO%20GRANDE%20.png?raw=true' />
             </div>
-            <div >
+            <div className='flowersRigth' >
                 <img className='principalHomeFlowersimg' alt='flowersRigth' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20RIGTH.png?raw=true' />
             </div>
         </div>
