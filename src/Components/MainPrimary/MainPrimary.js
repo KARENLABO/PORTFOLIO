d41c7074737738b66e306082c068c8834fd71304
Karen Ramirez <karen.bussines@hotmail.com>
2020-05-10 20:25:26 -0500

    se cambia la imagen de la flores left y se da estilo a la pagina

--- src/Components/MainPrimary/MainPrimary.js
+++ src/Components/MainPrimary/MainPrimary.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import './MainPrimary.css'
 
 function MainPrimary() {
     return (
-        <div>
-            <img alt='ProfilePhoto' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/LOGO%20KAREN%20GRANDE.png?raw=true' />
-            <img alt='flowersLeft' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20LEFT.png?raw=true' />
-            <img alt='flowersRigth' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20RIGTH.png?raw=true' />
+        <div className='principalHome'>
+            <div>
+                <img alt='flowersLeft' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20LEFT.png?raw=true' />
+            </div>
+            <div>
+                <img alt='ProfilePhoto' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/LOGO%20GRANDE%20.png?raw=true' />
+
+            </div>
+            <div>
+                <img alt='flowersRigth' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20RIGTH.png?raw=true' />
+            </div>
         </div>
     )
 }
