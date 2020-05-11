5158c1fdf60ce46855ce0b9928d27d19fd50374b
Karen Ramirez <karen.bussines@hotmail.com>
2020-05-10 20:43:38 -0500

    cambio el tamaño de las flores left y rigth

--- src/Components/MainPrimary/MainPrimary.js
+++ src/Components/MainPrimary/MainPrimary.js
@@ -4,15 +4,14 @@ import './MainPrimary.css'
 function MainPrimary() {
     return (
         <div className='principalHome'>
-            <div>
-                <img alt='flowersLeft' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20LEFT.png?raw=true' />
+            <div >
+                <img className='principalHomeFlowersimg' alt='flowersLeft' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20LEFT.png?raw=true' />
             </div>
-            <div>
+            <div className='logoprincipalHome'>
                 <img alt='ProfilePhoto' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/LOGO%20GRANDE%20.png?raw=true' />
-
             </div>
-            <div>
-                <img alt='flowersRigth' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20RIGTH.png?raw=true' />
+            <div >
+                <img className='principalHomeFlowersimg' alt='flowersRigth' src='https://github.com/KARENLABO/PORTFOLIO/blob/master/src/img/FLORES%20RIGTH.png?raw=true' />
             </div>
         </div>
     )
