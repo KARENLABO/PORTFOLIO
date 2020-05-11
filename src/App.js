5a94e5732dd4016e707cae397d78a33882cd0a48
FelixDeLaCruzTR <64222312+FelixDeLaCruzTR@users.noreply.github.com>
2020-05-10 19:36:07 -0500

    INICIALIZANDO MAINPRIMARY

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainPrimary from './Components/MainPrimary/MainPrimary';
 
 function App() {
     return (
         <div className="App">
-            <p> Hola </p>
+            <MainPrimary/>
         </div >
     );
 }
