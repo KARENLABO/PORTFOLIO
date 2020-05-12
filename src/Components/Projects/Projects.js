003dc3ec7bebc1a39e73d76740d799f7b4f0d61d
Karen Ramirez <karen.bussines@hotmail.com>
2020-05-12 00:11:51 -0500

    se suben cambios de supuesto projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -11,30 +11,20 @@ function Projects({ option }) {
             <MiniNavBar opt={option} />
             <div className='containerProjects'>
                 <div>
-                    <img className='project' alt='project' src='https://github.com/KARENLABO/GDL004-lab-notes/blob/develop/lab-notes/src/Images/Data%20lovers.JPG?raw=true' />
-                    <br></br>
-                    <img alt='puntos' src={GreenPoints} />
-                    <p>DATA LOVERS</p>
-                </div>
-                <div>
-                    <img className='project' alt='project' src='https://github.com/KARENLABO/GDL004-lab-notes/blob/develop/lab-notes/src/Images/LAB%20NOTES%20SIGN%20IN.JPG?raw=true' />
-                    <br></br>
-                    <img alt='puntos' src={GreenPoints} />
-                    <p>LAB NOTES</p>
-                </div>
-                <div>
-                    <img className='project' alt='project' src='https://github.com/KARENLABO/GDL004-movie-challenge/blob/master/page-movies/src/Components-CSS/img/homefinal.png?raw=true' />
-                    <br></br>
-                    <img alt='puntos' src={GreenPoints} />
-                    <p>MOVIE CHALLENGE</p>
-                </div>
-                <div>
-                    <img className='project' alt='project' src='https://raw.githubusercontent.com/KARENLABO/GDL004-cipher/master/src/styles/PROTOTIPO%20FINAL.jpeg' />
-                    <br></br>
-                    <img alt='puntos' src={GreenPoints} />
-                    <p>CIPHER</p>
+                    <img className='project4' alt='project' src='https://raw.githubusercontent.com/KARENLABO/GDL004-cipher/master/src/styles/PROTOTIPO%20FINAL.jpeg' />
                 </div>
             </div>
+            <div>
+                <img className='project1' alt='project' src='https://github.com/KARENLABO/GDL004-lab-notes/blob/develop/lab-notes/src/Images/Data%20lovers.JPG?raw=true' />
+
+            </div>
+            <div>
+                <img className='project2' alt='project' src='https://github.com/KARENLABO/GDL004-lab-notes/blob/develop/lab-notes/src/Images/LAB%20NOTES%20SIGN%20IN.JPG?raw=true' />
+            </div>
+            <div>
+                <img className='project3' alt='project' src='https://github.com/KARENLABO/GDL004-movie-challenge/blob/master/page-movies/src/Components-CSS/img/homefinal.png?raw=true' />
+            </div>
+
         </div>
     );
 }
