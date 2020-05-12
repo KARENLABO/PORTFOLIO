a474c6b24dbdffd4b23fe8c180ff9cbe74d94d82
FelixDeLaCruzTR <64222312+FelixDeLaCruzTR@users.noreply.github.com>
2020-05-12 00:08:52 -0500

    Adding Contact component & resources

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import './Contact.css';
+import LinkedIn from '../../img/linkedin.svg';
+import GitHub from '../../img/github.svg';
+import Outlook from '../../img/outlook.svg';
 
 function Contact() {
     return (
-        <div>
-            <p>Contact....XD </p>
+        <div className='generalContact'>
+            <div className='c1'>
+                <div className='LinkedInIcon'>
+                    <img src={LinkedIn} className="linkedinIcon" alt="LinkedInLogo" />
+                </div>
+            </div>
+            <div className='c2'>
+                <div className='icon'>
+                    <img src={GitHub} className="gitHubIcon" alt="GitHubLogo" />
+                </div>
+            </div>
+            <div className='c3'>
+                <div className='OutlookImg'>
+                    <img src={Outlook} className="OutlookIcon" alt="OutlookLogo" />
+                </div>
+            </div>
         </div>
     )
 }
