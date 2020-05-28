aaec66db6fc638a34ea179f8ba3b302437601bdd
Felix De la Cruz Toledo <felix.delacruz@scalablepress.com>
2020-05-27 20:01:17 -0500

    se termina la parte de movile del oveview

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -10,24 +10,24 @@ function Contact() {
     return (
         <div className='generalContact'>
 
-            <a href='https://www.linkedin.com/in/akarenramirez/' className='c1'>
+            <a href='https://www.linkedin.com/in/akarenramirez/' className='linkedindiv'>
                 <div className='LinkedInIcon'>
                     <img src={LinkedIn} className="linkedinIcon" alt="LinkedInLogo" />
                 </div>
             </a>
 
-            <a href='https://github.com/KARENLABO' className='c2'>
+            <a href='https://github.com/KARENLABO' className='githubdiv'>
                 <div className='icon'>
                     <img src={GitHub} className="gitHubIcon" alt="GitHubLogo" />
                 </div>
             </a>
-            <div className='c3'>
-                <div className='OutlookImg'>
+            <div className='emaildiv'>
+                <div className='Outlookicon'>
                     <img src={Email} className="OutlookIcon" alt="OutlookLogo" />
                 </div>
             </div>
-            <div className='c4'>
-                <div className='phoneNumber'>
+            <div className='phonediv'>
+                <div className='phoneNumbericon'>
                     <img src={Phone} className="PhoneIcon" alt="PhoneLogo" />
                 </div>
             </div>
