89126b762cd9993e11ab5c11d54217bad7b683fa
Karen Ramirez <karen.bussines@hotmail.com>
2020-05-29 18:22:15 -0500

    se agrega al resume una clase

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -2,18 +2,18 @@ import React from 'react';
 import MiniNavBar from '../MiniNavBar/MiniNavBar';
 import './Resume.css';
 import resume2 from '../../pdf/resume.pdf';
-import PDFViewer from 'pdf-viewer-reactjs'
+// import PDFViewer from 'pdf-viewer-reactjs'
 
 function Resume({ option }) {
     return (
         <div>
-            <MiniNavBar opt={option}/>
-            <PDFViewer
+            <MiniNavBar opt={option} />
+            {/* <PDFViewer
                 document={{
-                    url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
-                }}
-            />
-            
+                url: 'https://arxiv.org/pdf/quant-ph/0410100.pdf',
+            }}
+            /> */}
+
         </div>
     );
 }
