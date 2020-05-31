f9331c246ca27117ab12dd97f16f0d603e2149c7
Felix De la Cruz Toledo <felix.delacruz@scalablepress.com>
2020-05-30 20:59:23 -0500

    se corrige el overview para que no se necesite darle  un scroll cuando se accese al el

--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -25,12 +25,12 @@ function Overview({ option }) {
                         
                         My name is Ana Karen Ramirez Nuñez, I love to create functional and Beautiful websites. Developed by Figma, VS Code and coffee.
                         <br></br>
+                        
                        
                         <p className='aniqestionsText'>
                             Got any questions? Contact me at :
                          </p>
-                        <br></br>
-                        <br></br>
+
                     </div>
                 </div>
                 <div className='contactmeSocialNetworks'>
