50c1c161d8d0fa45172593ca4e570bdc903018ef
Karen Ramirez <karen.bussines@hotmail.com>
2020-06-17 20:30:42 -0500

    agregando mas texto a overview y haciendo resposivo el texto

--- src/Components/Overview/Overview.js
+++ src/Components/Overview/Overview.js
@@ -11,7 +11,7 @@ function Overview({ option }) {
 
     return (
         <div>
-            <MiniNavBar2/>
+            <MiniNavBar2 />
             {/* <MiniNavBar opt={option} /> */}
             <div className='containerOverview'>
                 <div className='divProfilePhoto'>
@@ -24,25 +24,27 @@ function Overview({ option }) {
                         <p className='tracking-in-expand'>
                             Hello!
                         </p>
+                        <p>
+                            My name is Karen Ramirez, I am 26 years old and I am currently a front-end developer, but I also studied a career in business development and innovation, so I also have a technical career in Human Resources Management.
+                        </p>
 
-                        My name is Ana Karen Ramirez Nuñez, I love to create functional and Beautiful websites. Developed by using  Figma, VS Code and coffee.
-                        <br></br>
+                        <p>
+                            8 months ago I decided to enter the world of programming and discovered that this is my passion, which is why I am looking for a job opportunity as a front-end jr developer.
+                        </p>
+
+                        <p>
+                            I know there is still a long way to go, but it motivates me to continue to be a better version of myself every day.
+                        </p>
+
 
                         <div className='contactinfo'>
-                        <br></br>
                             <h3 className='aniqestionsText'>
-                                Got any questions? Contact me at : 
-                               
-                         
-                             </h3>
+                                Got any questions? Contact me at :
+                            </h3>
 
-                             <p>
-                             (33 14 95 62 88)     or
+                            <p>
+                                (33 14 95 62 88)     or
                             </p>
-                            <br></br>
-
-                      
-
                         </div>
 
 
