78055b9e51f70cebe96ad62fb882ffd30f4174d9
Karen Ramirez <karen.bussines@hotmail.com>
2020-06-23 20:13:51 -0500

    arreglando el tamaño del pdf para movile

--- src/Components/Backends/PDFJs/PDFJs.js
+++ src/Components/Backends/PDFJs/PDFJs.js
@@ -3,7 +3,7 @@ export default function PDFJs(source, element) {
     const iframe = document.createElement('iframe');
     iframe.src = `/pdfjs/web/viewer.html?file=${source}`;
     iframe.width = '85%';
-    iframe.height = '850px';
+    iframe.height = '450px';
 
 
 
