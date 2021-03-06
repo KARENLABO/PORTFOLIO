6f1b40022a3b458bf87d64a2a0d142e077bc9331
Karen Ramirez <karen.bussines@hotmail.com>
2021-03-05 23:32:08 -0600

    refactoring Code, beginig changing the Main page of the app

--- src/Components/MainPrimary/MainPrimary.js
+++ src/Components/MainPrimary/MainPrimary.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import TazaYellowimg from '../../img/TazaYellow.svg'
+import CirculoGris from '../../img/CirculoGris.svg'
 import './MainPrimary.css';
-import LogoGrande from '../../img/LOGO GRANDE.png';
+
 
 
 
@@ -9,13 +11,35 @@ function MainPrimary() {
     return (
         <div className='principalHome'>
 
-            <div className='bounce-in-top'>
-                <img alt='ProfilePhoto' className='ProfilePhoto' src={LogoGrande} />
-            </div>
-
             <div className='navbarhome'>
                 <NavBar />
             </div>
+
+            <div className='ContainerMainPage'>
+
+                <div className='Karen-Ramirez-txt'>
+                    <h1> KAREN </h1>
+                    <h1> RAMIREZ </h1>
+                        <h3> Jr. Front End Developer </h3>
+                </div>
+
+
+
+                <div className='containerTazaAndElements'>
+                    <div className='Taza-Yellow'>
+                        <img alt='TazaYellow' className='Taza-Yellow-img' src={TazaYellowimg} />
+                    </div>
+
+            
+                    
+                    <div className='click-to-discover-txt'>
+                        <p> Click to </p>
+                        <p> Discover </p>
+                    </div>
+                </div>
+    
+            </div>
+
 
 
         </div>
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -10,23 +10,17 @@ function NavBar() {
             <li class="nav-item">
                 <Link class="nav-link active" to='/overview'>
                     <h3 className='textNavPrincipal'>OVERVIEW</h3>
-                    <img alt='greenBranch' className='greenBranch' src={greenBranch} />
-
                 </Link>
             </li>
             <li class="nav-item">
                 <Link class="nav-link active" to='/resume'>
                     <h3 className='textNavPrincipal'>RESUME</h3>
-                    <img alt='greenBranch' className='greenBranch' src={greenBranch} />
-
-
                 </Link>
             </li>
             <li class="nav-item">
                 <Link class="nav-link active" to='/projects'>
                     <h3 className='textNavPrincipal'>PROJECTS</h3>
-                    <img alt='greenBranch' className='greenBranch' src={greenBranch} />
-
+                    
                 </Link>
             </li>
 
